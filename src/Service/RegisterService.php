857c5ee002cb74dc8405759bae9da4e7d765da1a
tlugat <teo.lugat@hetic.net>
2023-02-04 16:14:32 +0100

    ✨ Add user authentication

---
+++ src/Service/RegisterService.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use DateTimeImmutable;
+use Doctrine\ORM\EntityManagerInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Ramsey\Uuid\Uuid;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Twig\Environment;
+
+class RegisterService
+{
+    public function __construct(UserPasswordHasherInterface $passwordHasher, MailService $mailService, EntityManagerInterface $em, Environment $twig, UserRepository $userRepository) {
+        $this->passwordHasher = $passwordHasher;
+        $this->mailService = $mailService;
+        $this->em = $em;
+        $this->twig = $twig;
+        $this->userRepository = $userRepository;
+    }
+    public function register(User $user): void {
+        $token = Uuid::uuid4();
+        $date = new DateTimeImmutable('now');
+        $hash = $this->passwordHasher->hashPassword($user, $user->getPassword());
+        $user->setPassword($hash);
+        $user->setCreatedAt($date);
+        $user->setToken($token);
+        $user->setRoles(['ROLE_USER']);
+        $this->em->persist($user);
+        $this->em->flush();
+        $this->sendEmailConfirmation($user, $token);
+    }
+
+    private function sendEmailConfirmation(User $user, string $token): void {
+        $emailTemplate = $this->twig->render('emails/accountVerification.html.twig', [
+            'token' =>  $token,
+            'firstname' => $user->getFirstname()
+        ]);
+        $this->mailService->sendMail([
+            'sender' => array('name' => 'ESGI', 'email' => 'ali.fatoori@gmail.com'),
+            'htmlContent' => $emailTemplate,
+            'to' => array(
+                array('email' => $user->getEmail(), 'name' => "{$user->getFirstname()} {$user->getLastname()}")
+            ),
+            'subject' => 'Email confirmation'
+        ]);
+    }
+
+    public function checkUserToken(string $token) {
+        return $this->userRepository->findOneBy(['token' => $token]);
+    }
+}
